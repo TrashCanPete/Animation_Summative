a5d395614cc75ce0c71358169a56175c6d1c0bd2
Peter McDermott <mcdermpete@gmail.com>
2019-11-28 16:03:18 +1300

    Movement set up, states set up

--- Animation Summative/Assets/Scripts/BabyController.cs
+++ Animation Summative/Assets/Scripts/BabyController.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class BabyController : MonoBehaviour
 {
+    public float moveSpeed;
     public Animator cameraAnim;
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        anim.SetBool("SitDown", false);
+        anim.SetBool("Walking", false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            anim.SetBool("Walking", true);
+            anim.SetBool("SitDown", false);
+            characterScale.x = 1;
+            transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            anim.SetBool("Walking", true);
+            anim.SetBool("SitDown", false);
+            characterScale.x = -1;
+            transform.Translate(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetAxis("Horizontal") == 0)
+        {
+            anim.SetBool("Walking", false);
+            anim.SetBool("SitDown", true);
+        }
         
+                transform.localScale = characterScale;
+
+
     }
 
     void CameraShake()
