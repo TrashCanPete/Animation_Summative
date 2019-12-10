20a7678e2e46ef2212843fb8478fc1f311ce7c1e
Peter McDermott <mcdermpete@gmail.com>
2019-12-11 11:45:00 +1300

    Imported Animations

---
+++ Animation Summative/Assets/AZRIController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AZRIController : MonoBehaviour
+{
+    Animator anim;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            anim.SetTrigger("DrawSword");
+            
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            anim.SetTrigger("Sheath");
+        }
+    }
+}
